373793c330b5096b0f44e9bd84475a902fdfccb8
Audie80 <audie.menut@gmail.com>
2019-07-18 14:45:28 +0200

    initialisation

---
+++ routes/routes.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var mongodb = require('../db');
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  mongodb.getVal(res);
+});
+
+router.post('/values', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  var val = req.body.value;
+
+  if (val === undefined || val === "") {
+    res.send(JSON.stringify({status: "error", value: "Value undefined"}));
+    return
+  }
+  mongodb.sendVal(val, res);
+});
+
+router.delete('/values/:id', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  var uuid = req.params.id;
+
+  if (uuid === undefined || uuid === "") {
+    res.send(JSON.stringify({status: "error", value: "UUID undefined"}));
+    return
+  }
+  mongodb.delVal(uuid);
+  res.send(JSON.stringify({status: "ok", value: uuid}));
+});
+
+module.exports = router;
